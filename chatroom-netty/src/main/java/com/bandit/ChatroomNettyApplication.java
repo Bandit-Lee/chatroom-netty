d57e4235c4ed4631fbc8d16cc50cf9f60a466dc2
bandit <2577569090@qq.com>
2022-06-28 15:58:54 +0800

    init the project

---
+++ chatroom-netty/src/main/java/com/bandit/ChatroomNettyApplication.java
@@ -0,0 +1,13 @@
+package com.bandit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatroomNettyApplication.class, args);
+    }
+
+}
---
+++ chatroom-netty/src/test/java/com/bandit/ChatroomNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
